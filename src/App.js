a7b7efa85d74353f487e2ffed79901188a725bd9
Theodore Stansbury <tstansbu.27@gmail.com>
2022-06-02 05:13:26 -0400

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react'
 import './App.css';
 import Die from './Die'
+import { nanoid } from 'nanoid';
 
 export default function App() {
-  const [dice, setDice] = React.useState(diceNum())
+  const [dice, setDice] = React.useState(allDice())
 
-  function diceNum() {
+  function generateNewDice() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function allDice() {
     const dieNum = []
     for (let i = 0; i < 10; i++) {
-      dieNum.push(Math.floor(Math.random() * 6) + 1)
+      dieNum.push(generateNewDice())
     }
     return dieNum
   }
 
   function rollDice() {
-    setDice(diceNum())
+    setDice(prevDice => prevDice.map(die => {
+      return !die.isHeld ? generateNewDice() : die      
+    }))
+  }
+
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return id === die.id ?
+      { ...die, isHeld: !die.isHeld } : die
+    }))
   }
 
   return (
     <main className="App">
       <div className="game-container">
-        <Die dice={dice}/>
+        <Die dice={dice} hold={holdDice}/>
         <button className="roll-button" onClick={rollDice}>Roll</button>
       </div>
     </main>
--- src/Die.js
+++ src/Die.js
@@ -2,10 +2,16 @@ import React from 'react'
 import './Die.css'
 
 export default function Die(props) {
-    const dieElements = props.dice.map((num, index) => {
+    const dieElements = props.dice.map(num => {
+        const styles = {backgroundColor: num.isHeld ? "#59E391" : "#FFF"}
         return (
-            <div className="dice" key={index}>
-                <h3>{num}</h3>
+            <div
+                className="dice"
+                style={styles}
+                onClick={() => props.hold(num.id)}
+                key={num.id}
+            >
+                <h3>{num.value}</h3>
             </div>
         )
     })
